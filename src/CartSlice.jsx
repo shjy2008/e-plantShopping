ea1a5f28f135c228e9cedf97a79e5350f4a193c3
shjy2008 <shjy2011@163.com>
2025-01-18 14:50:31 +0800

    CartSlice, total cart number

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,25 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const plantItem = action.payload;
+      const existingItem = state.items.find((item) => item == plantItem);
+      if (existingItem) {
+        existingItem["quantity"] += 1;
+      }
+      else {
+        state.items.push({"item": plantItem, "quantity": 1});
+      }
     },
     removeItem: (state, action) => {
+      const plantItem = action.payload;
+      state.items = state.items.filter((item) => item != plantItem);
     },
     updateQuantity: (state, action) => {
-
-    
+      const {plantItem, amount} = action.payload;
+      const existingItem = state.item.find((item) => item == plantItem);
+      if (existingItem) {
+        existingItem["quantity"] = amount;
+      }
     },
   },
 });
